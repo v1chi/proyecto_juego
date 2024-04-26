3a20ffa28465625bbba9241456123af0da10d975
Cr1stianNb <cristiannettle1@gmail.com>
2024-04-26 16:09:10 -0400

    Cambios en carpeta cartas
    Mejore los contenedores de las cartas

---
+++ Carpeta Cartas/Escena Selección/Panel.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_gui_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if get_rect().has_point(event.global_position):
+			print("Click")
+
+
+
+
+func _on_mouse_entered():
+	print("AAAAAA")
+	set_self_modulate(Color(0, 0, 0, 1))
+
+
+
+func _on_mouse_exited():
+	set_self_modulate(Color(255, 255, 255, 0))
+
