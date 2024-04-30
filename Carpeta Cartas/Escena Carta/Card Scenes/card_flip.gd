d5a6c6dd862d84a7f8edf95c9da414e6cff9bd4e
Cr1stianNb <cristiannettle1@gmail.com>
2024-04-30 12:49:24 -0400

    Animación de desplazamiento de cartas

--- Carpeta Cartas/Escena Carta/Card Scenes/card_flip.gd
+++ Carpeta Cartas/Escena Carta/Card Scenes/card_flip.gd
@@ -1,13 +1,19 @@
 extends Node2D
-
+var anim
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	var anim = $AnimationPlayer
+
+func activate_card_flip():
 	anim.play("card_flip")
+
+
+func _ready():	
+	anim = $AnimationPlayer
+	anim.play("shift")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
