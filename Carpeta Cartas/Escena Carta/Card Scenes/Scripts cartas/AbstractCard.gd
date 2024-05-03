7de4f2cfdcea63fe67c883442060ec460d248180
Cr1stianNb <cristiannettle1@gmail.com>
2024-05-03 12:16:30 -0400

    Cambios cartas

---
+++ Carpeta Cartas/Escena Carta/Card Scenes/Scripts cartas/AbstractCard.gd
@@ -0,0 +1,49 @@
+class_name AbstractCard 
+extends Node2D
+
+
+var imagen_carta
+var sprite_carta
+var anim
+
+signal card_created
+
+
+func on_card_created():
+	print("Carta creada ")
+	card_created.emit()
+
+
+
+func activar_efecto():
+	push_error("ERROR DE NO IMPLEMENTACIÓN DEL MÉTODO")
+
+func set_carta_frontal(texture_load):
+	assert(sprite_carta != null, "Nodo Sprite2D de la carta frontal es nulo")
+	sprite_carta.set_texture(texture_load)
+
+
+func activate_card_flip():
+	assert(anim != null, "Animation player es nulo")
+	anim.play("card_flip")
+	
+func activate_shift_animation():
+	assert(anim != null, "Animation player es nulo")
+	anim.play("shift")
+
+
+
+
+func _ready():
+	assert(get_node("AnimationPlayer") != null, "AnimationPlayer es nulo")
+	assert(get_node("cardback/frontal") != null, "Sprite2D de la carta frontal es nulo")
+	assert(imagen_carta != null, "Imagen de la carta es nulo")
+	
+	anim = get_node("AnimationPlayer")
+	sprite_carta = get_node("cardback/frontal")
+	
+	activate_shift_animation()
+	set_carta_frontal(imagen_carta)
+
+
+
---
+++ Carpeta Cartas/Escena Carta/Card Scenes/Scripts cartas/Carta1.gd
@@ -0,0 +1,11 @@
+extends AbstractCard
+
+var path_carta_frontal = "res://Carpeta Cartas/Escena Carta/CuteCards - asset pack/cartas_separadas/2.tres"
+
+func _ready():
+	super._ready()
+	print("Ready de la clase concreta")
+
+
+func _init(): 
+	self.imagen_carta = load(path_carta_frontal)
