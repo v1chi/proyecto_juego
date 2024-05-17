92d55cc778e825a95d559d638788f6c21d4906a0
GATOSEX0 <giannbruzon@gmail.com>
2024-05-17 18:14:35 -0400

    se chantan sonido

--- Menu/Pausa.gd
+++ Menu/Pausa.gd
@@ -1,9 +1,11 @@
 extends CanvasLayer
 
+@onready var pop = $AudioStreamPlayer
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("Pausa"):
 		get_tree().paused = not get_tree().paused
+		pop.play()
 		$ColorRect.visible = not $ColorRect.visible
 		$VBoxContainer/Continuar.visible = not $VBoxContainer/Continuar.visible
 		$VBoxContainer/SalidaMenu.visible = not $VBoxContainer/SalidaMenu.visible
@@ -14,8 +16,6 @@ var cambioSalida = preload("res://Menu/menu.tscn")
 func _on_salida_menu_pressed():
 	get_tree().change_scene_to_packed(cambioSalida)
 	
-	
-
 
 func _on_continuar_pressed():
 	pass # Replace with function body.
