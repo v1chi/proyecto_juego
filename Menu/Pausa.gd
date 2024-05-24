a745ed7ea7af2c94b71044b70a1e46a469643dfb
Menderin <vjopia2004@gmail.com>
2024-05-24 17:07:35 -0400

    Bug menu
    -se arregla el bug del menu
    -se esconde el boton opciones

--- Menu/Pausa.gd
+++ Menu/Pausa.gd
@@ -1,29 +1,27 @@
 extends CanvasLayer
 
 @onready var pop = $AudioStreamPlayer
+var cambioSalida = load("res://Menu/menu.tscn")
+
+func _ready():
+	$VBoxContainer/Continuar.connect("pressed", Callable(self, "_on_continuar_pressed"))
+	$VBoxContainer/SalidaMenu.connect("pressed", Callable(self, "_on_salida_menu_pressed"))
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("Pausa"):
-		get_tree().paused = not get_tree().paused
-		pop.play()
-		$ColorRect.visible = not $ColorRect.visible
-		$VBoxContainer/Continuar.visible = not $VBoxContainer/Continuar.visible
-		$VBoxContainer/SalidaMenu.visible = not $VBoxContainer/SalidaMenu.visible
-		$Label.visible = not $Label.visible
+		_toggle_pause()
 
-var cambioSalida = load("res://Menu/menu.tscn")
-
-func _on_salida_menu_pressed():
-	get_tree().change_scene_to_packed(cambioSalida)
-	
-	
-
-
-
-func _on_continuar_pressed():
+func _toggle_pause():
 	get_tree().paused = not get_tree().paused
+	pop.play()
 	$ColorRect.visible = not $ColorRect.visible
 	$VBoxContainer/Continuar.visible = not $VBoxContainer/Continuar.visible
 	$VBoxContainer/SalidaMenu.visible = not $VBoxContainer/SalidaMenu.visible
 	$Label.visible = not $Label.visible
 
+func _on_salida_menu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(cambioSalida)
+
+func _on_continuar_pressed():
+	_toggle_pause()
