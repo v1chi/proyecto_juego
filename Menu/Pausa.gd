4c62eb003c4d37d6c9718694c7c61bb647994b76
GATOSEX0 <giannbruzon@gmail.com>
2024-05-24 01:29:25 -0400

    boton continuar funciona

--- Menu/Pausa.gd
+++ Menu/Pausa.gd
@@ -11,11 +11,19 @@ func _physics_process(delta):
 		$VBoxContainer/SalidaMenu.visible = not $VBoxContainer/SalidaMenu.visible
 		$Label.visible = not $Label.visible
 
-var cambioSalida = preload("res://Menu/menu.tscn")
+var cambioSalida = load("res://Menu/menu.tscn")
 
 func _on_salida_menu_pressed():
 	get_tree().change_scene_to_packed(cambioSalida)
 	
+	
+
+
 
 func _on_continuar_pressed():
-	pass # Replace with function body.
+	get_tree().paused = not get_tree().paused
+	$ColorRect.visible = not $ColorRect.visible
+	$VBoxContainer/Continuar.visible = not $VBoxContainer/Continuar.visible
+	$VBoxContainer/SalidaMenu.visible = not $VBoxContainer/SalidaMenu.visible
+	$Label.visible = not $Label.visible
+
