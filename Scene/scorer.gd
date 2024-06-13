c1f92f4f9c60ae6f8beeb9ce840b4e70743d80d4
Cr1stianNb <cristiannettle1@gmail.com>
2024-06-13 17:49:22 -0400

    GUI SCORE

--- Scene/scorer.gd
+++ Scene/scorer.gd
@@ -2,13 +2,22 @@ extends Control
 
 
 var text_score
-
+var interval_score
+var current_score
+var anim : AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	text_score = $RichTextLabel
+	current_score = 0
+	anim = $AnimationPlayer
+	text_score = $ParentPanel/Panel/RichTextLabel
 	Global.update_score.connect(_on_update_score)
 	pass # Replace with function body.
 
+
+
+func wait(seconds):
+	await get_tree().create_timer(seconds).timeout
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,5 +25,25 @@ func _process(delta):
 
 
 func _on_update_score():
-	text_score.text = "Score: " + str(Global.score)
-	print(Global.score)
+	var inter = 10
+	var cont = 0
+	interval_score = range(current_score, Global.score, inter)
+	for i in interval_score:
+		text_score.text = str(i)
+		anim.play("add")
+		if cont % 2  == 0:
+			cont += 1
+			await anim.animation_finished
+		else:
+			await wait(0.05)
+		
+	current_score = Global.score 
+	text_score.text = str(current_score)
+	
+
+
+
+
+func _on_button_pressed(arg):
+	Global.score_agregate(arg)
+
