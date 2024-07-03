f6b967290b63d05843adb1c5f677cfbd7bd919ec
Liespren <kevins.o@outlook.com>
2024-07-03 18:50:12 -0400

    depurando

--- Characters/Enemies/Mage/Zonas de spawn.gd
+++ Characters/Enemies/Mage/Zonas de spawn.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 signal emitir_efecto
 
-var particula = "res://Characters/Enemies/Mague/Efectos_mago/mageAttack.tscn"
+var particula = "res://Characters/Enemies/Mage/Efectos_mago/mageAttack.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
